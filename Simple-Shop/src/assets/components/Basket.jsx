72eeb266a391033abc4ed2003f438c23be31ecee
Gareth Davies <mrgarethddavies@gmail.com>
2022-11-11 11:06:55 +0100

    cart total works

--- Simple-Shop/src/assets/components/Basket.jsx
+++ Simple-Shop/src/assets/components/Basket.jsx
@@ -1,10 +1,18 @@
 function Basket(props) {
+    function getTotal(){
+        let total = 0;
+        props.cart.forEach(item => {
+            total += item.price * item.amount;
+        });
+        return total;
+    }
   return (
     <section className="Basket">
         <ul>
             {props.cart.map(item => 
                 <li>{item.productdisplayname} x {item.amount}, {item.price*item.amount},-</li>)}
             </ul>
+            <h1>Total: {getTotal()},-</h1>
             <button>Buy now plz</button>
     </section>
   )
